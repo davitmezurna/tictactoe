3b28ab4ce02e2fbe676362d3ab7658d2b9cb1987
hp <davit.mezurnishvili.1@btu.edu.ge>
2020-11-18 16:34:18 +0400

    tictactoe

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.tictactoe
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,60 +13,81 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         init()
+        buttonreset.setOnClickListener {
+            button00.text = ""
+            button00.isClickable = true
+            button01.text = ""
+            button01.isClickable = true
+            button02.text = ""
+            button02.isClickable = true
+            button10.text = ""
+            button10.isClickable = true
+            button11.text = ""
+            button11.isClickable = true
+            button12.text = ""
+            button12.isClickable = true
+            button20.text = ""
+            button20.isClickable = true
+            button21.text = ""
+            button21.isClickable = true
+            button22.text = ""
+            button22.isClickable = true
+        }
     }
 
     private fun init() {
         button00.setOnClickListener {
-          changeButton (button00)
+            changeButton (button00)
         }
         button01.setOnClickListener() {
-         changeButton(button01)
+            changeButton(button01)
         }
 
         button02.setOnClickListener() {
-         changeButton(button02)
+            changeButton(button02)
         }
         button10.setOnClickListener() {
-         changeButton(button10)
+            changeButton(button10)
         }
         button11.setOnClickListener() {
-         changeButton(button11)
+            changeButton(button11)
         }
         button12.setOnClickListener() {
-         changeButton(button12)
+            changeButton(button12)
         }
         button20.setOnClickListener() {
-         changeButton(button20)
+            changeButton(button20)
         }
         button21.setOnClickListener() {
-         changeButton(button21)
+            changeButton(button21)
         }
         button22.setOnClickListener() {
-         changeButton(button22)
+            changeButton(button22)
         }
         buttonreset.setOnClickListener() {
 
         }
 
     }
-    private fun changeButton (button:Button) {
+    private fun changeButton (button: Button) {
         if (isPlayerOne) {
             button.text = "X"
         }else {
             button.text = "O"
         }
-        button.isclickable = false
-        isPlayerOne = isPlayerOne
+        button.isClickable = false
+        isPlayerOne =! isPlayerOne
         checkWinner ()
 
-    }
+    } //ძალიან დიდი ბოდიში ვერ მოვასწარი კოდის დასრულება ტექნიკური ხარვეზების გამო რამდენჯერმე
+    //მომიწია ანდროიდ სტუდიოს წაშლა და ხელახლა ჩაწერა
     private fun checkWinner () {
-        if (button00.text.isnotempty() && button00.text.toString() == button01.text.toString() && button00.text.toString() == button02.text.toString()) {
-            resultTextView.text = "winner is $ (button00.text)"
-            buttonContainer.visibility = View.Gone
-        }else if (button10.text.isnotempty() && button10.text.toString() == button11.text.toString() && button10.text.toString() == button12.text.toString()) {
-            resultTextView.text = "winner is $ (button00.text)"
-            buttonContainer.visibility = View.Gone
+        if (button00.text.isNotEmpty() && button00.text.toString() == button01.text.toString() && button00.text.toString() == button02.text.toString()) {
+            Toast.makeText(this , "Winner is ${button00.text}", Toast.LENGTH_SHORT).show()
+
+        }else if (button10.text.isNotEmpty() && button10.text.toString() == button11.text.toString() && button10.text.toString() == button12.text.toString()) {
+            Toast.makeText(this , "Winner is ${button10.text}", Toast.LENGTH_SHORT).show()
+
         }
 
     }
